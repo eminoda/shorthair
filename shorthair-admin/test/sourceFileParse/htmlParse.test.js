afb073180943505720efd0afc3110640781e61a5
sxh@9niu.com <245978782@qq.com>
2019-02-26 15:50:02 +0800

    chore #templateNodes parseTo html

--- shorthair-admin/test/sourceFileParse/htmlParse.test.js
+++ shorthair-admin/test/sourceFileParse/htmlParse.test.js
@@ -1,58 +1,31 @@
-const jsdom = require("jsdom");
+const path = require('path');
+const fs = require('fs');
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const dom = new JSDOM(`<html></html>`);
 const document = dom.window.document;
-const templateNodes = require("../../mock/templateNodes.json");
+const templateNodes = require('../../mock/templateNodes.json');
 
-let totlaHtml = "";
+let totlaHtml = '';
 
-let r = generateELementNodes(document.createElement("span"), templateNodes);
-console.log(r.outerHTML);
+let rootElement = document.createElement('body');
+generateELementNodes(rootElement, templateNodes);
+
+fs.writeFileSync(path.join(__dirname, 'result.html'), rootElement.outerHTML);
 
 function generateELementNodes(element, templateNodes) {
-  if (templateNodes) {
-    for (let node of templateNodes) {
-      if (node.nodeType == 1) {
-        let childElement = document.createElement(node.tag.toLocaleLowerCase());
-        childElement.className = node.className;
-        console.log(childElement.outerHTML);
-        const next = generateELementNodes(childElement, node.nodes);
-        console.log(next);
-        next && childElement.appendChild(next);
-        element.appendChild(childElement);
-        console.log(element.outerHTML);
-      }
-      if (node.nodeType == 3) {
-        let childElement = document.createTextNode(node.text);
-        element = element.appendChild(childElement);
-      }
-    }
-    return element;
-  }
-  return "";
+	if (templateNodes) {
+		for (let node of templateNodes) {
+			if (node.nodeType == 1) {
+				let childElement = document.createElement(node.tag.toLocaleLowerCase());
+				childElement.className = node.className;
+				generateELementNodes(childElement, node.nodes);
+				element.appendChild(childElement);
+			}
+			if (node.nodeType == 3) {
+				let childElement = document.createTextNode(node.text);
+				element.appendChild(childElement);
+			}
+		}
+	}
 }
-
-// function buildHtml(nodes) {
-//   let currentLevel = "";
-//   for (const node of nodes) {
-//     if (node.nodeType == 1) {
-//       let element = document.createElement(node.tag.toLocaleLowerCase());
-//       element.className = node.className;
-//       let htmlStr = element.outerHTML;
-//       console.log(htmlStr);
-//       let startHtmlStr = htmlStr.match(
-//         /^<[a-z]+(\s+[a-z]+="([a-z]+|([\-]*[a-z])\s*)*")+>/
-//       )[0];
-//       let middleStr = buildHtml(node.nodes);
-//       const endTagRe = /<\/[a-z]+>$/;
-//       const matchEndTag = htmlStr.match(endTagRe);
-//       const endHtmlStr = matchEndTag ? matchEndTag[0] : "";
-//       currentLevel + startHtmlStr + middleStr + endHtmlStr;
-//     } else {
-//       currentLevel + node.text;
-//     }
-//   }
-//   return currentLevel;
-// }
-
-// buildHtml(templateNodes);
