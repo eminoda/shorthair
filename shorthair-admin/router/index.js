24f1bb91759de0407368ca060ff28a79dd307cf1
eminoda <245978782@qq.com>
2019-02-28 01:42:24 +0800

    add #router

--- shorthair-admin/router/index.js
+++ shorthair-admin/router/index.js
@@ -1,6 +1,12 @@
-const compose = require("koa-compose");
-const templateRouter = require("./templateRouter");
-module.exports = compose([
-  templateRouter.routes(),
-  templateRouter.allowedMethods()
-]);
+// const compose = require("koa-compose");
+// const templateRouter = require("./templateRouter");
+// module.exports = compose([
+//   templateRouter.routes(),
+//   templateRouter.allowedMethods()
+// ]);
+module.exports = app => {
+	const { router } = app;
+	router.get('/template', async (ctx, next) => {
+		ctx.body = 123;
+	});
+};
