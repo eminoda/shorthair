2ca5d1ec68839396745650efac638400d6da7b4b
sxh@9niu.com <245978782@qq.com>
2019-03-19 10:51:11 +0800

    build #menu

--- shorthair-app/src/app/app.component.ts
+++ shorthair-app/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, RouterEvent } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { Menu } from './interface/menu';
-import { Menus } from './interface/menus';
 
 @Component({
   selector: 'app-root',
@@ -10,34 +9,45 @@ import { Menus } from './interface/menus';
 })
 export class AppComponent {
   title = 'shorthair-app';
-  activeUrl: any;
-  menus: Array<Menus> = [
+  activeUrl: string;
+  activeId: number;
+  menus: Array<Menu> = [
     {
+      id: 10,
       name: '站点',
+      icon: 'cloud',
       subMenus: [
         {
+          id: 11,
           name: '域名设置',
           url: '/website/list'
         },
         {
+          id: 12,
           name: '站点解析',
           url: '/website/mapper'
         }
       ]
     },
     {
+      id: 20,
       name: '页面',
+      icon: 'profile',
       subMenus: [
         {
+          id: 21,
           name: '页面管理',
           url: '/page/list'
         }
       ]
     },
     {
+      id: 30,
       name: '模板',
+      icon: 'table',
       subMenus: [
         {
+          id: 31,
           name: '模板管理',
           url: '/template/list'
         }
@@ -46,10 +56,22 @@ export class AppComponent {
   ];
 
   constructor(router: Router) {
+    // https://angular.io/api/router/Event
     router.events.subscribe(
-      (data: RouterEvent) => {
-        if (data.url) {
+      (data: NavigationEnd) => {
+        if (data.url && data instanceof NavigationEnd) {
           this.activeUrl = data.url;
+          for (let menu of this.menus) {
+            const menuId = menu.id;
+            for (let submenu of menu.subMenus) {
+              if (this.activeUrl === submenu.url) {
+                const subId = submenu.id;
+                if (Math.round(subId / 10) === menuId / 10) {
+                  this.activeId = menuId;
+                }
+              }
+            }
+          }
         }
       },
       err => {}
--- shorthair-app/src/app/interface/menus.ts
+++
@@ -1,7 +0,0 @@
-import { Menu } from './menu';
-
-export interface Menus {
-  name: string;
-  url?: string;
-  subMenus: Array<Menu>;
-}
